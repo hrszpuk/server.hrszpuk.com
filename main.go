5dd4a1b7a50bf095c47eda12087ab38688046bd7
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-02-19 13:31:32 +0000

    Set up main routes

--- main.go
+++ main.go
@@ -3,26 +3,51 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"log"
+	"server.hrszpuk.com/api"
 )
 
 func main() {
+	log.Printf("Setting up server")
+
+	log.Printf("Doing preemptive checks")
+	DirectoryStatusCheck(api.ContainersDir)
+	DirectoryStatusCheck(api.ScriptsDir)
+
 	app := fiber.New()
 
-	v1 := app.Group("v1")
+	log.Printf("Setting up routes")
 
-	v1.Get("docs", docs)
-	v1.Get("ping", ping)
-	v1.Get("uptime", uptime)
-	v1.Get("os", os)
+	// Server website routes
+	app.Get("/", home)
+
+	// Server API routes
+	apiGroup := app.Group("api")
+	v1 := apiGroup.Group("v1")
+
+	v1.Get("ping", api.Ping)
+	v1.Get("uptime", api.Uptime)
+	v1.Get("system", api.System)
 
 	containers := v1.Group("containers")
 
-	containers.Get("list", containersList)
-	containers.Get("get/:id", containersGet)
+	containers.Get("list", api.ContainersList)
+	containers.Get("get/:id", api.ContainersGet)
 
 	scripts := v1.Group("scripts")
-	scripts.Get("list", scriptsList)
-	scripts.Get("run/:id", scriptsRun)
 
+	scripts.Get("list", api.ScriptsList)
+	scripts.Get("run/:id", api.ScriptsRun)
+
+	log.Printf("Starting server. Listening on port 3000.")
 	log.Fatal(app.Listen(":3000"))
+}
+
+func home(ctx *fiber.Ctx) error {
+	err := ctx.SendFile("pages/home.html")
+	if err == nil {
+		log.Printf("Served home.html to %s", ctx.IP())
+	} else {
+		log.Printf("An error occurred while trying to serve home.html to %s. Error: %s", ctx.IP(), err.Error())
+	}
+	return nil
 }
