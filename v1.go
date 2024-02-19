bd6111b3891d1fa9ca542094efbe141ed6d78e65
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-02-19 09:39:16 +0000

    Added basic v1 code

---
+++ v1.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func docs(ctx *fiber.Ctx) error {
+
+}
+
+func ping(ctx *fiber.Ctx) error {
+
+}
+
+func uptime(ctx *fiber.Ctx) error {
+
+}
+
+func os(ctx *fiber.Ctx) error {
+
+}
