7db1b9cc09c9ac8e89cbdff5eb3183c0644e46a1
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-02-19 13:31:12 +0000

    Added api/containers.go

---
+++ api/containers.go
@@ -0,0 +1,13 @@
+package api
+
+import "github.com/gofiber/fiber/v2"
+
+const ContainersDir = "containers"
+
+func ContainersList(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func ContainersGet(ctx *fiber.Ctx) error {
+	return nil
+}
