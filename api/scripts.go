96a4be1d99760f157692b3c540cd539a5a1bf4a6
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-02-19 13:31:18 +0000

    Added api/scripts.go

---
+++ api/scripts.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+const ScriptsDir = "scripts"
+
+func ScriptsList(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func ScriptsRun(ctx *fiber.Ctx) error {
+	return nil
+}
