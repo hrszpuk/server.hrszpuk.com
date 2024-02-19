05132b3fbef17c7406e0da96628d1e806fb7145e
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-02-19 13:29:07 +0000

    Moved v1.go to /api

---
+++ api/v1.go
@@ -0,0 +1,15 @@
+package api
+
+import "github.com/gofiber/fiber/v2"
+
+func Ping(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func Uptime(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func System(ctx *fiber.Ctx) error {
+	return nil
+}
